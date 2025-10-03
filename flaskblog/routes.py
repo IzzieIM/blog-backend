26c0aa93a2558581a164b920105758148d44fcaf
IzzieIM <anushanikam05@kgpian.iitkgp.ac.in>
2025-10-03 14:56:18 +0530

    picture upload changes and logic

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,9 @@
+import secrets , os
+from PIL import Image
 from flaskblog.models import User, Post
 from flaskblog import app , db , bcrypt
 from flask import render_template , url_for , flash , redirect , request
-from flaskblog.forms import RegistrationForm , LoginForm
+from flaskblog.forms import RegistrationForm , LoginForm , UpdateAccountForm
 from flask_login import login_user , logout_user , login_required , current_user
 #we moved it down because it failed earlier beacuse it havent seen the db variable yet , not that it has seen it itll no longer fail
 
@@ -70,8 +72,40 @@ def logout_page():
     logout_user()
     return redirect(url_for('home_page'))
 
-@app.route("/Account")
+
+def save_picture(form_picture):
+    # we are doing this to extract the filename and extention but the major importance is of extention
+    random_hex = secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+
+    # resize the image with pillow before we save it
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/Account" , methods = ['GET' , 'POST'])
 @login_required
 def account_page():
-    return render_template('account.html' , title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()  
+        flash('Your account has been updated!' , 'success')
+        return redirect(url_for('account_page'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static' , filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html' , title = 'Account' , image_file = image_file , form = form)
 
